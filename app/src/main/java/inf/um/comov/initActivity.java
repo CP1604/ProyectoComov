585287476bac4522de6ec8f1744d975296a630a2
AlvaroCava <81183612+AlvaroCava@users.noreply.github.com>
2021-04-05 22:30:54 +0200

    Intro Hecha

--- app/src/main/java/inf/um/comov/initActivity.java
+++ app/src/main/java/inf/um/comov/initActivity.java
@@ -2,25 +2,31 @@ package inf.um.comov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class initActivity extends AppCompatActivity {
+
+    private  final int DURACION_SPLASH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
 
-        ProgressBar pb = findViewById(R.id.simpleProgressBar);
-        for(int i = 0; i< 100; i++){
-            try {
-                wait(1000);
-                pb.setProgress(i);
-            } catch (InterruptedException w){
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        setContentView(R.layout.activity_init);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent  intent = new Intent(initActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
-        }
+        }, DURACION_SPLASH);
     }
-
 }
